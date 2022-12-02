00c829004bbed2630026fd8d056dce306451e37d
zetsuen <kevnhalim@gmail.com>
2022-12-02 16:25:13 +0700

    Adding Tile Type and Enemy's Pathfinding

--- Victual Heroes/src/vh/object/MapTile.java
+++ Victual Heroes/src/vh/object/MapTile.java
@@ -5,13 +5,19 @@ import java.awt.image.BufferedImage;
 public class MapTile {
 
 	private BufferedImage map;
+	private int tileType;
 	
-	public MapTile(BufferedImage map) {
+	public MapTile(BufferedImage map, int tileType) {
 		this.map = map;
+		this.tileType = tileType;
 	}
 
 	public BufferedImage getMap() {
 		return map;
+	}
+
+	public int getTileType() {
+		return tileType;
 	}
 	
 }
